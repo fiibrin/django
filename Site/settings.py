aecbe99acb9ee719a91513ddb8ee0ad44d88bab5
fiibrin <stabp8103@gmail.com>
2023-05-27 16:38:10 +0400

    0.1.5 version restoran web app

--- Site/settings.py
+++ Site/settings.py
@@ -99,10 +99,7 @@ WSGI_APPLICATION = 'Site.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': 'postgresql://postgres:NHjNPRGC8xWHhKbVpOxu@containers-us-west-163.railway.app:7774/railway',
 }
 
 
