6699080dc5621e6f4e65efec8d73987c12652feb
fiibrin <stabp8103@gmail.com>
2023-05-27 16:49:40 +0400

    0.1.5 version restoran web app

--- Site/settings.py
+++ Site/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'Site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'NHjNPRGC8xWHhKbVpOxu',
+        'HOST': 'containers-us-west-163.railway.app',
+        'PORT': '7774'
     }
 }
 
