676818dbf81bbb078c31907ee342ed16a919bf90
fiibrin <stabp8103@gmail.com>
2023-05-27 16:31:10 +0400

    0.1.5 version restoran web app

--- Site/settings.py
+++ Site/settings.py
@@ -11,18 +11,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, ".env"))
-SECRET_KEY = 'django-insecure-7m!3mojt*xqc22qk6ry0=dz&^6w&#d#ay$8xk$$r602h1dnb_$'
-SECRET_KEY = os.getenv("SECRET_KEY")
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-7m!3mojt*xqc22qk6ry0=dz&^6w&#d#ay$8xk$$r602h1dnb_$'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-7m!3mojt*xqc22qk6ry0=dz&^6w&#d#ay$8xk$$r602h1dnb_$')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
@@ -95,18 +98,13 @@ WSGI_APPLICATION = 'Site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
